1a31f56938cb909a0b4068aa394cc0b7a9a313a3
unknown <guillaume.sabin@gmail.com>
2018-11-15 10:47:29 +0100

    "db_connection_modif"

--- resources/db.sql
+++ resources/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 23 août 2018 à 09:03
+-- Généré le :  jeu. 15 nov. 2018 à 09:32
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.2.4
 
@@ -30,19 +30,23 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `author`;
 CREATE TABLE IF NOT EXISTS `author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `number` int(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
-  `photo` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `photo` varchar(255) NOT NULL,
+  `linkedin` varchar(255) NOT NULL,
+  `github` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `author`
 --
 
-INSERT INTO `author` (`firstname`, `lastname`, `number`, `mail`, `photo`) VALUES
-('Guillaume', 'Sabin', 777310018, 'guillaume.sabin@gmail.com', 'photo-cv.jpg');
+INSERT INTO `author` (`id`, `firstname`, `lastname`, `number`, `mail`, `photo`, `linkedin`, `github`) VALUES
+(2, 'Guillaume', 'Sabin', 777310018, 'guillaume.sabin@gmail.com', 'photo-cv.jpg', 'https://www.linkedin.com/in/guillaume-sabin-348285160/', 'https://github.com/guillaume-sabin');
 
 -- --------------------------------------------------------
 
@@ -56,6 +60,9 @@ CREATE TABLE IF NOT EXISTS `website` (
   `url` text NOT NULL,
   `description` varchar(100) NOT NULL,
   `name` varchar(60) NOT NULL,
+  `languages` text NOT NULL,
+  `frameworks` text NOT NULL,
+  `technical_description` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
@@ -63,12 +70,11 @@ CREATE TABLE IF NOT EXISTS `website` (
 -- Déchargement des données de la table `website`
 --
 
-INSERT INTO `website` (`id`, `url`, `description`, `name`) VALUES
-(1, 'blog.png', 'illustration du projet blog', 'Blog'),
-(3, 'restaurant.png', 'projet de fast-food en ligne', 'Restaurant'),
-(4, 'adresse.png', 'Création d\'un carnet d\'adresse dynamique', 'Carnet d\'adresse'),
-(5, 'todolist.png', 'Création d\'un block note dynamique', 'Todolist'),
-(6, 'ardoise.png', 'Création d\'une ardoise magique', 'Ardoise magique');
+INSERT INTO `website` (`id`, `url`, `description`, `name`, `languages`, `frameworks`, `technical_description`) VALUES
+(1, 'blog.png', 'illustration du projet blog', 'Blog', 'HTML, PHP, SQL, CSS', 'Aucun', 'L\'idée était de créer un blog, sans l\'utilisation de framework, uniquement avec des languages natifs.\r\nLa plus grosse partie se situant au niveau des requêtes SQL, ainsi que la manipulation de ces données avec PHP.\r\nCe blog contient également un système de commentaire pour les différents articles, ainsi qu\'un back office qui permet l\'administration du site.'),
+(3, 'fastfood.png', 'projet de fast-food en ligne', 'Restaurant', 'HTML, PHP, CSS, JavaScript', 'jQuery, Micro-framework PHP homemade de la 3WA', 'Première manipulation d\'un micro-framework PHP, on retrouve sur ce site un système de création de compte ainsi que la possibilité de commander différents produits avec un système de \"panier\" enregistrer côté client et/ou en base de donnée. L\'utilisation du concept de programmation AJAX à également été nécessaire pour la manipulation du \"panier\".'),
+(4, 'adressbook.png', 'Création d\'un carnet d\'adresse dynamique', 'Carnet d\'adresse', 'HTML, CSS, JavaScript', 'Aucun', 'L\'une de mes première réalisation consistait à créer un carnet d\'adresse qui soit stocké dans le LocalStorage côté client.'),
+(6, 'magicslate.png', 'Création d\'une ardoise magique', 'Ardoise magique', 'HTML, CSS, JavaScript', 'jQuery', 'Le concept est plutôt simple et loin d\'être nouveau, la création d\'une ardoise, où l\'on peut modifier l\'épaisseur du trait, sa couleur ainsi que gommer.\r\nUne sorte de \"Paint\" en somme, l’intérêt principale était la manipulation de l\'élément canvas pour un rendu dynamique à l\'aide de JavaScript.');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- resources/db_pgsql.sql
+++ resources/db_pgsql.sql
@@ -1,4 +1,5 @@
 CREATE TABLE author (
+  id int(11) NOT NULL AUTO_INCREMENT,
   firstname varchar(255) NOT NULL,
   lastname varchar(255) NOT NULL,
   number integer NOT NULL,
@@ -6,17 +7,20 @@ CREATE TABLE author (
   photo varchar(255) NOT NULL
 );
 
-INSERT INTO author VALUES ('Guillaume', 'Sabin', 777310018, 'guillaume.sabin@gmail.com', 'photo-cv.jpg');
+INSERT INTO author VALUES (2, 'Guillaume', 'Sabin', 777310018, 'guillaume.sabin@gmail.com', 'photo-cv.jpg', 'https://www.linkedin.com/in/guillaume-sabin-348285160/', 'https://github.com/guillaume-sabin');
 
 CREATE TABLE website (
   id  SERIAL PRIMARY KEY,
   url text NOT NULL,
   description varchar(100) NOT NULL,
-  name varchar(60) NOT NULL
+  name varchar(60) NOT NULL,
+  languages text NOT NULL,
+  frameworks text NOT NULL,
+  technical_description text NOT NULL,
+  PRIMARY KEY (id)
 );
 
-INSERT INTO website VALUES (1, 'blog.png', 'illustration du projet blog', 'Blog');
-INSERT INTO website VALUES (3, 'restaurant.png', 'projet de fast-food en ligne', 'Restaurant');
-INSERT INTO website VALUES (4, 'adresse.png', 'Création d''un carnet d''adresse dynamique', 'Carnet d''adresse');
-INSERT INTO website VALUES (5, 'todolist.png', 'Création d''un block note dynamique', 'Todolist');
-INSERT INTO website VALUES (6, 'ardoise.png', 'Création d''une ardoise magique', 'Ardoise magique');
+INSERT INTO website VALUES (1, 'blog.png', 'illustration du projet blog', 'Blog', 'HTML, PHP, SQL, CSS', 'Aucun', 'L\'idée était de créer un blog, sans l\'utilisation de framework, uniquement avec des languages natifs.\r\nLa plus grosse partie se situant au niveau des requêtes SQL, ainsi que la manipulation de ces données avec PHP.\r\nCe blog contient également un système de commentaire pour les différents articles, ainsi qu\'un back office qui permet l\'administration du site.');
+INSERT INTO website VALUES (3, 'fastfood.png', 'projet de fast-food en ligne', 'Restaurant', 'HTML, PHP, CSS, JavaScript', 'jQuery, Micro-framework PHP homemade de la 3WA', 'Première manipulation d\'un micro-framework PHP, on retrouve sur ce site un système de création de compte ainsi que la possibilité de commander différents produits avec un système de \"panier\" enregistrer côté client et/ou en base de donnée. L\'utilisation du concept de programmation AJAX à également été nécessaire pour la manipulation du \"panier\".');
+INSERT INTO website VALUES (4, 'adressbook.png', 'Création d\'un carnet d\'adresse dynamique', 'Carnet d\'adresse', 'HTML, CSS, JavaScript', 'Aucun', 'L\'une de mes première réalisation consistait à créer un carnet d\'adresse qui soit stocké dans le LocalStorage côté client.');
+INSERT INTO website VALUES (6, 'magicslate.png', 'Création d\'une ardoise magique', 'Ardoise magique', 'HTML, CSS, JavaScript', 'jQuery', 'Le concept est plutôt simple et loin d\'être nouveau, la création d\'une ardoise, où l\'on peut modifier l\'épaisseur du trait, sa couleur ainsi que gommer.\r\nUne sorte de \"Paint\" en somme, l’intérêt principale était la manipulation de l\'élément canvas pour un rendu dynamique à l\'aide de JavaScript.');
