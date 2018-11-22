ac4ddd217fa0d7fe19e5a9fc065efe48ec5e4fe4
Guillaume <guillaume.sabin@gmail.com>
2018-11-22 14:16:54 +0100

    modified ajax request

--- public/assets/js/classes/Content.js
+++ public/assets/js/classes/Content.js
@@ -6,6 +6,16 @@ var Content = function(tag)
     this.timer = 200;
 }
 
+Content.prototype.getContent = function(data)
+{
+    var self = this;
+    $.getJSON(
+        data.url,
+        data.id,
+        self.showContent.bind(self)
+    );
+}
+
 Content.prototype.showContent = function(jsonData)
 {
     const IMGLINK = 'assets/img/';
@@ -44,51 +54,4 @@ Content.prototype.showContent = function(jsonData)
         $('#' + this.DOMElement.id).fadeTo(this.timer, 1);
         document.getElementById('ws-container').style.boxShadow = "inset 0px 0px 20px 6px rgba(0,0,0,0.75)";
     }     
-}
-
-Content.prototype.getContent = function(data)
-{
-    var self = this;
-    $.getJSON(
-        data.url,
-        data.id,
-        function(){
-            const IMGLINK = 'assets/img/';
-            console.log('inside showContent method');
-            if(this.DOMElement.nodeName == 'P')
-            {
-                // Create a new container <img> and set his attributes
-                var newHtmlElement = document.createElement('img');
-                newHtmlElement.id = 'website';
-                newHtmlElement.src = IMGLINK + jsonData.url;
-                newHtmlElement.alt = jsonData.description;
-                newHtmlElement.dataset.wsId = jsonData.id;
-                
-                // The "#ws-container" was <p> container, no replace with <img>
-                this.DOMElement.replaceWith(newHtmlElement);
-        
-                // Show the new container
-                $('#' + this.DOMElement.id).fadeTo(this.timer, 1);
-                document.getElementById('ws-container').style.boxShadow = "inset 0px 0px 20px 6px rgba(0,0,0,0.75)";
-            }
-        
-            // Check if the clicked link is a new one
-            else if(this.DOMElement.dataset.wsId != jsonData.id)
-            {
-                // Hide the container before setting new params
-                $('#' + this.DOMElement.id).fadeTo(this.timer, 0);
-                
-                // Delay the container's settings
-                setTimeout((function(){
-                    this.DOMElement.setAttribute('src', IMGLINK + jsonData.url);
-                    this.DOMElement.setAttribute('alt', jsonData.description);
-                    this.DOMElement.dataset.wsId = jsonData.id;
-                }).bind(this), this.timer);
-        
-                // Show the container 
-                $('#' + this.DOMElement.id).fadeTo(this.timer, 1);
-                document.getElementById('ws-container').style.boxShadow = "inset 0px 0px 20px 6px rgba(0,0,0,0.75)";
-            } 
-        }.bind(self)
-    );
 }
