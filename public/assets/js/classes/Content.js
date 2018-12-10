842f563ece32f3a9c577120b53c148263407867e
Guillaume <guillaume.sabin@gmail.com>
2018-12-10 15:37:05 +0100

    animation_fixed

--- public/assets/js/classes/Content.js
+++ public/assets/js/classes/Content.js
@@ -38,10 +38,12 @@ Content.prototype.getContent = function(data)
             // Hide the container before setting new params
             $('#' + this.DOMElement.id).fadeTo(this.timer/2, 0, function(){
 
-                this.DOMElement.setAttribute('src', IMGLINK + jsonData.url);
+                setTimeout(function(){
+                    this.DOMElement.setAttribute('src', IMGLINK + jsonData.url);
                 this.DOMElement.setAttribute('alt', jsonData.description);
                 this.DOMElement.dataset.wsId = jsonData.id;
-
+                }.bind(this), 0);
+                
                 // Show the container 
                 $('#' + this.DOMElement.id).fadeTo(this.timer*4, 1, function(){
                     document.getElementById('ws-container').style.boxShadow = "inset 0px 0px 20px 6px rgba(0,0,0,0.75)";
@@ -50,44 +52,3 @@ Content.prototype.getContent = function(data)
         }     
     }.bind(this));
 }
-/*
-Content.prototype.showContent = function(jsonData)
-{
-    const IMGLINK = 'assets/img/';
-
-    if(this.DOMElement.nodeName == 'P')
-    {
-        // Create a new container <img> and set his attributes
-        var newHtmlElement = document.createElement('img');
-        newHtmlElement.id = 'website';
-        newHtmlElement.src = IMGLINK + jsonData.url;
-        newHtmlElement.alt = jsonData.description;
-        newHtmlElement.dataset.wsId = jsonData.id;
-        
-        // The "#ws-container" was <p> container, no replace with <img>
-        this.DOMElement.replaceWith(newHtmlElement);
-
-        // Show the new container
-        $('#' + this.DOMElement.id).fadeTo(this.timer, 1);
-        document.getElementById('ws-container').style.boxShadow = "inset 0px 0px 20px 6px rgba(0,0,0,0.75)";
-    }
-
-    // Check if the clicked link is a new one
-    else if(this.DOMElement.dataset.wsId != jsonData.id)
-    {
-        // Hide the container before setting new params
-        $('#' + this.DOMElement.id).fadeTo(this.timer, 0, function(){
-            this.DOMElement.setAttribute('src', IMGLINK + jsonData.url);
-            this.DOMElement.setAttribute('alt', jsonData.description);
-            this.DOMElement.dataset.wsId = jsonData.id;
-        }.bind(this));
-
-        window.setTimeout(function(){
-
-            // Show the container 
-            $('#' + this.DOMElement.id).fadeTo(this.timer*4, 1);
-            document.getElementById('ws-container').style.boxShadow = "inset 0px 0px 20px 6px rgba(0,0,0,0.75)"
-        }.bind(this), this.timer)
-    }     
-}
-*/
